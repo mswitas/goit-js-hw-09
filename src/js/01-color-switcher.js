e4ca51657a2accf519f3b8cd7a8c491eeec08553
mswitas <mswitas@gmail.com>
2023-12-07 12:22:10 +0100

    add task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,20 @@
+const startButton = document.querySelector('[data-start]');
+const stopButton = document.querySelector('[data-stop');
+let intervalId = null;
 
+startButton.addEventListener('click', () => {
+    startButton.setAttribute('disabled', '');
+    intervalId = setInterval(() => {
+        const bgColorHex = getRandomHexColor();
+        document.body.style.backgroundColor = bgColorHex;
+    }, 1000);
+});
+
+stopButton.addEventListener('click', () => {
+    clearInterval(intervalId);
+    startButton.removeAttribute('disabled');
+});
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
